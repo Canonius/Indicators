5f40da2042b4f2b335664660211616fcb8b08313
Canonius <50411138+Canonius@users.noreply.github.com>
2025-09-27 15:34:43 +0200

    farben angepasst

--- GoldenEra_Sessions/GoldenEra_Sessions/CustomIndicator.cs
+++ GoldenEra_Sessions/GoldenEra_Sessions/CustomIndicator.cs
@@ -3,11 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Sessions
 {
@@ -18,6 +13,20 @@ namespace Sessions
 
         [Input(Name = "Number of days")]
         public int NumberOfDays = 3;
+
+        // Steuerung: Sperren/Selektierbarkeit & Rand
+        [Input(Name = "Lock session objects")]
+        public bool LockObjects = true;
+
+        [Input(Name = "Objects selectable?")]
+        public bool SelectableObjects = false;
+
+        [Input(Name = "Force border to chart background color (fallback)")]
+        public bool ForceBorderToBackground = false;
+
+        [Input(Name = "Chart background color (fallback)")]
+        public Color ChartBackground = Color.Black;
+
         [Separator(Label = "A_Session parameters")]
         public string Separator_A_Session;
         [Input(Name = "A_Session start hour")]
@@ -29,7 +38,7 @@ namespace Sessions
         [Input(Name = "A_Session end minute")]
         public MinutesOfDay A_SessionEndMinute = MinutesOfDay.Minute_30;
         [Input(Name = "A_Session color")]
-        public Color A_SessionColor = Color.Gray;
+        public Color A_SessionColor = Color.LightYellow;
 
         [Separator(Label = "B_Session parameters")]
         public string Separator_B_Session;
@@ -42,7 +51,7 @@ namespace Sessions
         [Input(Name = "B_Session end minute")]
         public MinutesOfDay B_SessionEndMinute = MinutesOfDay.Minute_0;
         [Input(Name = "B_Session color")]
-        public Color B_SessionColor = Color.Gray;
+        public Color B_SessionColor = Color.Orange;
 
         [Separator(Label = "C_Session parameters")]
         public string Separator_USA;
@@ -91,7 +100,7 @@ namespace Sessions
         {
             DeleteObjects();
 
-            // The colors are set to be semi-transparent by changing the Alpha channel
+            // halbtransparente Füllfarben
             A_SessionColor = Color.FromArgb(100, A_SessionColor);
             B_SessionColor = Color.FromArgb(100, B_SessionColor);
             NYColor = Color.FromArgb(100, NYColor);
@@ -99,17 +108,10 @@ namespace Sessions
 
             for (int i = 0; i < NumberOfDays; i++)
             {
-                if (ShowA_Session)
-                    CreateObjects("TO" + i, A_SessionColor);
-
-                if (ShowB_Session)
-                    CreateObjects("LO" + i, B_SessionColor);
-
-                if (ShowNY)
-                    CreateObjects("NY" + i, NYColor);
-
-                if (ShowD_Session)
-                    CreateObjects("SY" + i, D_SessionColor);
+                if (ShowA_Session) CreateRectangle("TO" + i, A_SessionColor);
+                if (ShowB_Session) CreateRectangle("LO" + i, B_SessionColor);
+                if (ShowNY) CreateRectangle("NY" + i, NYColor);
+                if (ShowD_Session) CreateRectangle("SY" + i, D_SessionColor);
             }
 
             DrawSessions();
@@ -119,8 +121,6 @@ namespace Sessions
         {
             if (index != 0 || Period() >= (int)Timeframe.PERIOD_D1)
                 return;
-
-
 
             DrawSessions();
         }
@@ -132,19 +132,18 @@ namespace Sessions
             for (int i = 0; i < NumberOfDays; i++)
             {
                 if (ShowA_Session)
-                    DrawObjects(CurrentTime, "TO" + i, A_SessionStartHour, A_SessionStartMinute, A_SessionEndHour, A_SessionEndMinute);
+                    DrawRectangle(CurrentTime, "TO" + i, A_SessionStartHour, A_SessionStartMinute, A_SessionEndHour, A_SessionEndMinute);
 
                 if (ShowB_Session)
-                    DrawObjects(CurrentTime, "LO" + i, B_SessionStartHour, B_SessionStartMinute, B_SessionEndHour, B_SessionEndMinute);
+                    DrawRectangle(CurrentTime, "LO" + i, B_SessionStartHour, B_SessionStartMinute, B_SessionEndHour, B_SessionEndMinute);
 
                 if (ShowNY)
-                    DrawObjects(CurrentTime, "NY" + i, NYStartHour, NYStartMinute, NYEndHour, NYEndMinute);
+                    DrawRectangle(CurrentTime, "NY" + i, NYStartHour, NYStartMinute, NYEndHour, NYEndMinute);
 
                 if (ShowD_Session)
-                    DrawObjects(CurrentTime, "SY" + i, D_SessionStartHour, D_SessionStartMinute, D_SessionEndHour, D_SessionEndMinute);
+                    DrawRectangle(CurrentTime, "SY" + i, D_SessionStartHour, D_SessionStartMinute, D_SessionEndHour, D_SessionEndMinute);
 
                 CurrentTime = CurrentTime.AddDays(-1);
-                // We skip the weekends
                 while (CurrentTime.DayOfWeek == DayOfWeek.Saturday || CurrentTime.DayOfWeek == DayOfWeek.Sunday)
                     CurrentTime = CurrentTime.AddDays(-1);
             }
@@ -161,204 +160,145 @@ namespace Sessions
             }
         }
 
-        public void CreateObjects(string ObjectName, Color ObjectColor)
+        private void CreateRectangle(string name, Color fill)
         {
-            ObjectCreate(ObjectName, ObjectType.OBJ_RECTANGLE, DateTime.MinValue, 0, DateTime.MinValue, 0);
-            ObjectSet(ObjectName, ObjectProperty.OBJPROP_STYLE, LineStyle.STYLE_SOLID);
-            ObjectSet(ObjectName, ObjectProperty.OBJPROP_COLOR, ObjectColor);
+            ObjectCreate(name, ObjectType.OBJ_RECTANGLE, DateTime.MinValue, 0, DateTime.MinValue, 0);
+
+            // Füllfarbe (mit Alpha)
+            ObjectSet(name, ObjectProperty.OBJPROP_COLOR, fill);
+
+            // Umrandung entfernen:
+            ObjectSet(name, ObjectProperty.OBJPROP_STYLE, LineStyle.STYLE_SOLID);
+            ObjectSet(name, ObjectProperty.OBJPROP_WIDTH, 0); // keine Kante
+
+            // Fallback: ggf. Randfarbe auf Hintergrund setzen (falls Breite 0 nicht greift)
+            if (ForceBorderToBackground)
+                ObjectSet(name, ObjectProperty.OBJPROP_COLOR, Color.FromArgb(fill.A, ChartBackground));
+
+            // Sperren / Nicht auswählbar
+            ObjectSet(name, ObjectProperty.OBJPROP_LOCKED, LockObjects);
+            ObjectSet(name, ObjectProperty.OBJPROP_SELECTABLE, SelectableObjects);
         }
 
-        public void DrawObjects(DateTime CurrentDate, string ObjectName, HoursOfDay HourStart, MinutesOfDay MinuteStart, HoursOfDay HourEnd, MinutesOfDay MinuteEnd)
+        private void DrawRectangle(DateTime CurrentDate, string name, HoursOfDay HourStart, MinutesOfDay MinuteStart, HoursOfDay HourEnd, MinutesOfDay MinuteEnd)
         {
-            DateTime SessionStart, SessionEnd;
-
-            SessionStart = new DateTime(CurrentDate.Year, CurrentDate.Month, CurrentDate.Day).AddHours((int)HourStart).AddMinutes((int)MinuteStart);
-            SessionEnd = new DateTime(CurrentDate.Year, CurrentDate.Month, CurrentDate.Day).AddHours((int)HourEnd).AddMinutes((int)MinuteEnd);
+            DateTime SessionStart = new DateTime(CurrentDate.Year, CurrentDate.Month, CurrentDate.Day)
+                                        .AddHours((int)HourStart).AddMinutes((int)MinuteStart);
+            DateTime SessionEnd = new DateTime(CurrentDate.Year, CurrentDate.Month, CurrentDate.Day)
+                                        .AddHours((int)HourEnd).AddMinutes((int)MinuteEnd);
 
             double HighestValue = Highest(Symbol(), Period(), Series.MODE_HIGH, 1000, 0);
             double LowestValue = Lowest(Symbol(), Period(), Series.MODE_LOW, 1000, 0);
 
-            ObjectSet(ObjectName, ObjectProperty.OBJPROP_TIME1, SessionStart);
-            ObjectSet(ObjectName, ObjectProperty.OBJPROP_PRICE1, HighestValue * 1.5);
-            ObjectSet(ObjectName, ObjectProperty.OBJPROP_TIME2, SessionEnd);
-            ObjectSet(ObjectName, ObjectProperty.OBJPROP_PRICE2, LowestValue * 0.5);
+            ObjectSet(name, ObjectProperty.OBJPROP_TIME1, SessionStart);
+            ObjectSet(name, ObjectProperty.OBJPROP_PRICE1, HighestValue * 1.5);
+            ObjectSet(name, ObjectProperty.OBJPROP_TIME2, SessionEnd);
+            ObjectSet(name, ObjectProperty.OBJPROP_PRICE2, LowestValue * 0.5);
+
+            // Sicherstellen, dass Flags auch nach jedem Draw gesetzt bleiben:
+            ObjectSet(name, ObjectProperty.OBJPROP_LOCKED, LockObjects);
+            ObjectSet(name, ObjectProperty.OBJPROP_SELECTABLE, SelectableObjects);
+            ObjectSet(name, ObjectProperty.OBJPROP_WIDTH, 0);
+            if (ForceBorderToBackground)
+            {
+                // Füllfarbe holen, Alpha erhalten, aber Farbanteil = Hintergrund
+                // (Falls deine API separate Border-/Fill-Farben trennt, sag Bescheid, dann passe ich’s an.)
+                Color bg = ChartBackground;
+                // Color der Fläche bleibt via OBJPROP_COLOR gesetzt – hier kein zusätzlicher Call nötig,
+                // da wir oben bei CreateRectangle bereits den Fallback setzen.
+            }
         }
 
         public enum HoursOfDay
         {
-            [Description("0H")]
-            Hour_0 = 0,
-            [Description("1H")]
-            Hour_1 = 1,
-            [Description("2H")]
-            Hour_2 = 2,
-            [Description("3H")]
-            Hour_3 = 3,
-            [Description("4H")]
-            Hour_4 = 4,
-            [Description("5H")]
-            Hour_5 = 5,
-            [Description("6H")]
-            Hour_6 = 6,
-            [Description("7H")]
-            Hour_7 = 7,
-            [Description("8H")]
-            Hour_8 = 8,
-            [Description("9H")]
-            Hour_9 = 9,
-            [Description("10H")]
-            Hour_10 = 10,
-            [Description("11H")]
-            Hour_11 = 11,
-            [Description("12H")]
-            Hour_12 = 12,
-            [Description("13H")]
-            Hour_13 = 13,
-            [Description("14H")]
-            Hour_14 = 14,
-            [Description("15H")]
-            Hour_15 = 15,
-            [Description("16H")]
-            Hour_16 = 16,
-            [Description("17H")]
-            Hour_17 = 17,
-            [Description("18H")]
-            Hour_18 = 18,
-            [Description("19H")]
-            Hour_19 = 19,
-            [Description("20H")]
-            Hour_20 = 20,
-            [Description("21H")]
-            Hour_21 = 21,
-            [Description("22H")]
-            Hour_22 = 22,
-            [Description("23H")]
-            Hour_23 = 23,
+            [Description("0H")] Hour_0 = 0,
+            [Description("1H")] Hour_1 = 1,
+            [Description("2H")] Hour_2 = 2,
+            [Description("3H")] Hour_3 = 3,
+            [Description("4H")] Hour_4 = 4,
+            [Description("5H")] Hour_5 = 5,
+            [Description("6H")] Hour_6 = 6,
+            [Description("7H")] Hour_7 = 7,
+            [Description("8H")] Hour_8 = 8,
+            [Description("9H")] Hour_9 = 9,
+            [Description("10H")] Hour_10 = 10,
+            [Description("11H")] Hour_11 = 11,
+            [Description("12H")] Hour_12 = 12,
+            [Description("13H")] Hour_13 = 13,
+            [Description("14H")] Hour_14 = 14,
+            [Description("15H")] Hour_15 = 15,
+            [Description("16H")] Hour_16 = 16,
+            [Description("17H")] Hour_17 = 17,
+            [Description("18H")] Hour_18 = 18,
+            [Description("19H")] Hour_19 = 19,
+            [Description("20H")] Hour_20 = 20,
+            [Description("21H")] Hour_21 = 21,
+            [Description("22H")] Hour_22 = 22,
+            [Description("23H")] Hour_23 = 23,
         }
 
         public enum MinutesOfDay
         {
-            [Description("00")]
-            Minute_0 = 0,
-            [Description("01")]
-            Minute_1 = 1,
-            [Description("02")]
-            Minute_2 = 2,
-            [Description("03")]
-            Minute_3 = 3,
-            [Description("04")]
-            Minute_4 = 4,
-            [Description("05")]
-            Minute_5 = 5,
-            [Description("06")]
-            Minute_6 = 6,
-            [Description("07")]
-            Minute_7 = 7,
-            [Description("08")]
-            Minute_8 = 8,
-            [Description("09")]
-            Minute_9 = 9,
-            [Description("10")]
-            Minute_10 = 10,
-            [Description("11")]
-            Minute_11 = 11,
-            [Description("12")]
-            Minute_12 = 12,
-            [Description("13")]
-            Minute_13 = 13,
-            [Description("14")]
-            Minute_14 = 14,
-            [Description("15")]
-            Minute_15 = 15,
-            [Description("16")]
-            Minute_16 = 16,
-            [Description("17")]
-            Minute_17 = 17,
-            [Description("18")]
-            Minute_18 = 18,
-            [Description("19")]
-            Minute_19 = 19,
-            [Description("20")]
-            Minute_20 = 20,
-            [Description("21")]
-            Minute_21 = 21,
-            [Description("22")]
-            Minute_22 = 22,
-            [Description("23")]
-            Minute_23 = 23,
-            [Description("24")]
-            Minute_24 = 24,
-            [Description("25")]
-            Minute_25 = 25,
-            [Description("26")]
-            Minute_26 = 26,
-            [Description("27")]
-            Minute_27 = 27,
-            [Description("28")]
-            Minute_28 = 28,
-            [Description("29")]
-            Minute_29 = 29,
-            [Description("30")]
-            Minute_30 = 30,
-            [Description("31")]
-            Minute_31 = 31,
-            [Description("32")]
-            Minute_32 = 32,
-            [Description("33")]
-            Minute_33 = 33,
-            [Description("34")]
-            Minute_34 = 34,
-            [Description("35")]
-            Minute_35 = 35,
-            [Description("36")]
-            Minute_36 = 36,
-            [Description("37")]
-            Minute_37 = 37,
-            [Description("38")]
-            Minute_38 = 38,
-            [Description("39")]
-            Minute_39 = 39,
-            [Description("40")]
-            Minute_40 = 40,
-            [Description("41")]
-            Minute_41 = 41,
-            [Description("42")]
-            Minute_42 = 42,
-            [Description("43")]
-            Minute_43 = 43,
-            [Description("44")]
-            Minute_44 = 44,
-            [Description("45")]
-            Minute_45 = 45,
-            [Description("46")]
-            Minute_46 = 46,
-            [Description("47")]
-            Minute_47 = 47,
-            [Description("48")]
-            Minute_48 = 48,
-            [Description("49")]
-            Minute_49 = 49,
-            [Description("50")]
-            Minute_50 = 50,
-            [Description("51")]
-            Minute_51 = 51,
-            [Description("52")]
-            Minute_52 = 52,
-            [Description("53")]
-            Minute_53 = 53,
-            [Description("54")]
-            Minute_54 = 54,
-            [Description("55")]
-            Minute_55 = 55,
-            [Description("56")]
-            Minute_56 = 56,
-            [Description("57")]
-            Minute_57 = 57,
-            [Description("58")]
-            Minute_58 = 58,
-            [Description("59")]
-            Minute_59 = 59,
+            [Description("00")] Minute_0 = 0,
+            [Description("01")] Minute_1 = 1,
+            [Description("02")] Minute_2 = 2,
+            [Description("03")] Minute_3 = 3,
+            [Description("04")] Minute_4 = 4,
+            [Description("05")] Minute_5 = 5,
+            [Description("06")] Minute_6 = 6,
+            [Description("07")] Minute_7 = 7,
+            [Description("08")] Minute_8 = 8,
+            [Description("09")] Minute_9 = 9,
+            [Description("10")] Minute_10 = 10,
+            [Description("11")] Minute_11 = 11,
+            [Description("12")] Minute_12 = 12,
+            [Description("13")] Minute_13 = 13,
+            [Description("14")] Minute_14 = 14,
+            [Description("15")] Minute_15 = 15,
+            [Description("16")] Minute_16 = 16,
+            [Description("17")] Minute_17 = 17,
+            [Description("18")] Minute_18 = 18,
+            [Description("19")] Minute_19 = 19,
+            [Description("20")] Minute_20 = 20,
+            [Description("21")] Minute_21 = 21,
+            [Description("22")] Minute_22 = 22,
+            [Description("23")] Minute_23 = 23,
+            [Description("24")] Minute_24 = 24,
+            [Description("25")] Minute_25 = 25,
+            [Description("26")] Minute_26 = 26,
+            [Description("27")] Minute_27 = 27,
+            [Description("28")] Minute_28 = 28,
+            [Description("29")] Minute_29 = 29,
+            [Description("30")] Minute_30 = 30,
+            [Description("31")] Minute_31 = 31,
+            [Description("32")] Minute_32 = 32,
+            [Description("33")] Minute_33 = 33,
+            [Description("34")] Minute_34 = 34,
+            [Description("35")] Minute_35 = 35,
+            [Description("36")] Minute_36 = 36,
+            [Description("37")] Minute_37 = 37,
+            [Description("38")] Minute_38 = 38,
+            [Description("39")] Minute_39 = 39,
+            [Description("40")] Minute_40 = 40,
+            [Description("41")] Minute_41 = 41,
+            [Description("42")] Minute_42 = 42,
+            [Description("43")] Minute_43 = 43,
+            [Description("44")] Minute_44 = 44,
+            [Description("45")] Minute_45 = 45,
+            [Description("46")] Minute_46 = 46,
+            [Description("47")] Minute_47 = 47,
+            [Description("48")] Minute_48 = 48,
+            [Description("49")] Minute_49 = 49,
+            [Description("50")] Minute_50 = 50,
+            [Description("51")] Minute_51 = 51,
+            [Description("52")] Minute_52 = 52,
+            [Description("53")] Minute_53 = 53,
+            [Description("54")] Minute_54 = 54,
+            [Description("55")] Minute_55 = 55,
+            [Description("56")] Minute_56 = 56,
+            [Description("57")] Minute_57 = 57,
+            [Description("58")] Minute_58 = 58,
+            [Description("59")] Minute_59 = 59,
         }
-
     }
 }
